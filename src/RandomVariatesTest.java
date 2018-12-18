5d96c949d47c3fa620b4efa79ddb6b6b884ca6a6
Khaled Eldowa <khaled.eldowa3@gmail.com>
2018-12-18 13:19:47 +0200

    Histogram function, modified expo only

--- src/RandomVariatesTest.java
+++ src/RandomVariatesTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class RandomVariatesTest {
 
@@ -6,18 +7,46 @@ public class RandomVariatesTest {
 	public RandomVariatesTest() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	private void generateHistogram(int sampleSize, ArrayList<Double> samples, double min, double max)
+	{
+		int histogramSize = (int)Math.sqrt(sampleSize);
+		int[] histogram = new int[histogramSize];
+		double histogramIntervalWidth = (max-min)/histogramSize;
+		for(double value:samples)
+		{
+			histogram[(int)((value-min-Math.ulp(value))/histogramIntervalWidth)] += 1;
+		}
+		System.out.println("Histogram:");
+		for(int i=0; i<histogramSize; i++)
+		{
+			System.out.println("Interval " + i + ": " + histogram[i]);
+		}
+	}
 
 	void ExpTest(double mean) {
 		double testMean = 0.0;
+		double firstTest = rv.exponential(mean);
+		double max = firstTest;
+		double min = firstTest;
+		double temp;
 		int numberOfTests = 1000000;
-		for (int i = 0; i < 1000000; ++i) {
-			testMean += rv.exponential(mean);
+		ArrayList<Double> generatedList = new ArrayList<>();
+		generatedList.add(firstTest);
+		for (int i = 0; i < (numberOfTests-1); ++i) {
+			temp = rv.exponential(mean);
+			testMean += temp;
+			generatedList.add(temp);
+			if(temp>max)
+				max = temp;
+			else if(temp<min)
+				min = temp;
 		}
 		testMean /= numberOfTests;
 
 		System.out.println(
 				"Mean to be tested : " + Double.toString(rv.getMean()) + ", Measured Mean : " + Double.toString(testMean));
-
+		generateHistogram(numberOfTests, generatedList, min, max);		
 	}
 	
 	void logNormalTest(double mean, double variance) {
