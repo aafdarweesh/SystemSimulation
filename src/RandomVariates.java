60f821f7b3054db2f0d423b45d59fd5575a9147e
aafdarweesh <aafdarweesh@gmail.com>
2018-12-18 10:10:04 +0200

    TestError

--- src/RandomVariates.java
+++ src/RandomVariates.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 public class RandomVariates {
-	
+
 	private double mean;
 	private double variance;
-	Scanner c =new Scanner(System.in);
-	
-	RandomVariates(){
+	Scanner c = new Scanner(System.in);
+
+	RandomVariates() {
 		mean = 1.0;
 		variance = 1.0;
 	}
-	
+
 	public double uniform() {
 		double r;
 		do {
 			r = Math.random();
-		}while(r == 0.0 || r == 1.0);
+		} while (r == 0.0 || r == 1.0);
 		return r;
 	}
-	
 
 }
