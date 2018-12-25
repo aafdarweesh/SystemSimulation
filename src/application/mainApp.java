de0d40a9c0b05e917cbdd10cb543726251fa3e08
Khaled Eldowa <khaled.eldowa3@gmail.com>
2018-12-25 10:39:51 +0200

    Generators done, new packages.

--- src/application/mainApp.java
+++ src/application/mainApp.java
@@ -10,8 +10,10 @@ public class mainApp {
 	public static void main(String[] args) {
 		System.out.println("Now testing Expo:");
 		TestGenerator.test(new ExponentialGenerator(2.0));
+		
 		System.out.println("Now testing Lognormal:");
 		TestGenerator.test(new LogNormalGenerator(2.0, 1.0));
+		
 		System.out.println("Now testing weibull:");
 		TestGenerator.test(new WeibullGenerator(2.0, 1.0, 0.0));
 	}
