f433987a58a2eb845f772c12a656b0ab5992186a
aafdarweesh <aafdarweesh@gmail.com>
2019-01-02 18:52:23 +0200

    Remove unsued imports

--- src/application/mainApp.java
+++ src/application/mainApp.java
@@ -1,42 +1,36 @@
 package application;
 
-import javax.sql.rowset.spi.TransactionalWriter;
-
 import generator.PacketGenerator;
-import randomGens.ExponentialGenerator;
-import randomGens.LogNormalGenerator;
-import randomGens.RandomGenerator;
-import randomGens.TestGenerator;
-import randomGens.WeibullGenerator;
 import simulationModels.MMC;
-import simulationModels.Simulation;
 
 public class mainApp {
 
 	public static void main(String[] args) {
-		
-		/*System.out.println("Now testing Expo:");
-		TestGenerator.test(new ExponentialGenerator(2.0));
-		
-		System.out.println("Now testing Lognormal:");
-		TestGenerator.test(new LogNormalGenerator(2.0, 1.0));
-		
-		System.out.println("Now testing weibull:");
-		TestGenerator.test(new WeibullGenerator(2.0, 1.0, 0.0));*/
-		
-		/*RandomGenerator randomGenerator = new ExponentialGenerator(2);
-		for(int i=0; i<60; i++)
-			System.out.println(randomGenerator.generate());*/
-		
-		/*MMC trial = new MMC(2, 1, 3, 100);
-=======
-		/*MMC trial = new MMC(1, 1, 3, 100);
->>>>>>> refs/remotes/origin/TestMMC1.0
-		trial.startSimulation();
-		trial.showResult();*/
+
+		/*
+		 * System.out.println("Now testing Expo:"); TestGenerator.test(new
+		 * ExponentialGenerator(2.0));
+		 * 
+		 * System.out.println("Now testing Lognormal:"); TestGenerator.test(new
+		 * LogNormalGenerator(2.0, 1.0));
+		 * 
+		 * System.out.println("Now testing weibull:"); TestGenerator.test(new
+		 * WeibullGenerator(2.0, 1.0, 0.0));
+		 */
+
+		/*
+		 * RandomGenerator randomGenerator = new ExponentialGenerator(2); for(int i=0;
+		 * i<60; i++) System.out.println(randomGenerator.generate());
+		 */
+
+		/*
+		 * MMC trial = new MMC(1, 1, 3, 100);
+		 * 
+		 * trial.startSimulation(); trial.showResult();
+		 */
 		PacketGenerator pG = new PacketGenerator();
-		//pG.DisplayListOfJobs();
-		
+		// pG.DisplayListOfJobs();
+
 		MMC trial = new MMC(1, 1, 3, 100);
 		trial.startSimulation(pG.GeneratePackets(100, 3, 2));
 		trial.showResult();
